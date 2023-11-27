659779c963398550eab60d4776d2fe81758fb05b
melvin145 <pythonnoobie4@gmail.com>
2023-11-27 11:31:38 +0530

    j

---
+++ PROTM FRONTEND/script.js
@@ -0,0 +1,24 @@
+function ShowSidebar(toggle){
+      const sidebar=document.getElementById("sidebar");
+      sidebar.classList.toggle('show-sidebar');
+}
+
+const navbtn=document.querySelectorAll('.nav__btn');
+const slide=document.querySelectorAll('.slider__item');
+function ShowSlide(item){
+      navbtn.forEach((nav)=>{
+            nav.classList.remove('nav_active');
+      })
+      slide.forEach((s)=>{
+            s.classList.remove('active');
+      })
+      navbtn[item].classList.add('nav_active');
+      slide[item].classList.add('active');
+}
+
+navbtn.forEach((nav,i)=>{
+      console.log(nav);
+      nav.addEventListener('click',()=>{
+            ShowSlide(i);
+      })
+})
